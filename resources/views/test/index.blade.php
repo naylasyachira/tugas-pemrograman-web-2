fc8d97c74ebde629bdc951ce6e7b2584471edef0
Nayla Syachira <naylasyahirah12mm@gmail.com>
2026-04-18 14:39:28 +0800

    feat: implementasi fitur read untuk Movie

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    {{-- read feature --}}
     <div class="d-flex justify-content-between align-items-center mb-4">
         <h2>Daftar Movie</h2>
     </div>
